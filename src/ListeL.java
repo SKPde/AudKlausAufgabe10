84eb786e10cfa26e12b69430b1ae5f311b5a12fa
s.kuebel <s.kuebel@otterbach.de>
2017-08-15 10:16:05 +0200

    ssh Test

--- src/ListeL.java
+++ src/ListeL.java
@@ -8,7 +8,7 @@ public class ListeL {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo Welt!");
+		System.out.println("Hallo Welt!!");
 	}
 
 }
